0916fe69c8aaf2950f0f7a369160681c7fa2333c
dawnferre <ayj1002@naver.com>
2022-08-09 15:35:40 +0900

    08.09 JPA 원리 확인 및 적용, 테스트 - application.properties 설정이 안먹어서 쿼리가 안보임..추가 체크 필요.

---
+++ src/main/java/com/project/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.project.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //getter 메소드 자동생성
+@NoArgsConstructor //기본생성자 자동추가
+@Entity //테이블과 링크될 클래스 , 기본값으로 클래스 카멜케이스 = 테이블 명 매칭 ( StringbootMatching = stringboot_matching)
+public class Posts {
+    @Id //Pk필드 표시
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk의 생성 규칙, 뒤에 해당하는 옵션은 auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false) //문자열의 경우 기본은 VARCHAR(255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 빌더 패턴 클래스를 생성, 생성자 산단에 선언시 생성자 포함된 필드만 빌더에 포함.
+    public Posts(String title, String content, String author){
+        this.title=title;
+        this.content= content;
+        this.author= author;
+
+    }
+}
---
+++ src/test/java/com/project/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.project.springboot.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:/application.properties")
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach //단위테스트 끝난후 실행될 메소드 지정, 테스트용 데이터베이스인 H2에 데이터가 남아있으면 그다음 테스트 실패할 확률이 있음
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void loadingListData(){
+        //given
+        String title="테스트 게시글";
+        String content ="테스트 본문";
+        /**
+         * insert/update 실행
+         * - ID 값 존재하는 경우, update
+         * - ID 값 존재 X , insert
+         * */
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("yeongjuan.ensnc@gmail.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(),is(equalTo(title)));
+        assertThat(posts.getContent(),is(equalTo(content)));
+    }
+}
