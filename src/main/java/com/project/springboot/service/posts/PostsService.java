e670e6263b838b395d9018e6ed20d375146aa44d
dawnferre <ayj1002@naver.com>
2022-08-18 14:24:05 +0900

    08.11 chapter04. 머스테치로 화면구성하기 완료 - 등록 - 수정 - 삭제

--- src/main/java/com/project/springboot/service/posts/PostsService.java
+++ src/main/java/com/project/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.project.springboot.service.posts;
 
 import com.project.springboot.domain.posts.Posts;
 import com.project.springboot.domain.posts.PostsRepository;
+import com.project.springboot.web.dto.PostsListResponseDto;
 import com.project.springboot.web.dto.PostsResponseDto;
 import com.project.springboot.web.dto.PostsSaveRequestDto;
 import com.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly =true) //트랙재션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선 ( 조회기능만 있을경우, 추천)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)//람다식 응용 . == .map(posts -> new PostsListResponseDto(posts)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
